ccbc506e454a0af667d8e1676d98c7e062f810d6
draab <djamel.r.75@gmail.com>
2020-12-09 22:57:53 +0100

    update

--- dag_groupe6.py
+++ dag_groupe6.py
@@ -1,9 +1,9 @@
+import datetime as dt 
 from airflow import DAG 
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from apispotify import get_data
-import datetime as dt 
 
 now = dt.datetime.now() #- dt.timedelta(days=7)
 current_time = now.strftime('%Y%m%d')
@@ -28,38 +28,49 @@ dag = DAG ( 'dag_groupe6',
 
 with dag:
 
-    install = BashOperator(
-        task_id = "install",
-        bash_command = "pip install /root/airflow/dags/dag_nougatine/.",
-    ) 
+    # install = BashOperator(
+    #     task_id = "install",
+    #     bash_command = "pip install /root/airflow/dags/dag_nougatine/.",
+    # ) 
 
-    mkdir = BashOperator(
-        task_id = "mkdir",
-        bash_command = "mkdir /tmp/data_groupe6",
-    ) 
+    # mkdir = BashOperator(
+    #     task_id = "mkdir",
+    #     bash_command = "mkdir /tmp/data_groupe6",
+    # ) 
+
+    # get_data = PythonOperator(
+    #     task_id='get_data',
+    #     python_callable=get_data.launch,
+    #     op_kwargs={
+    #         'path':"/tmp/data_groupe6"
+    #     }
+    # )
 
     clean = BashOperator(
         task_id = "clean",
         bash_command = "rm -rf /tmp/data_groupe6",
     )
     
-    mkdir_dist = BashOperator(
-        task_id = "mkdir_dist",
-        bash_command = "hdfs dfs -mkdir /user/iabd2_group6/data/{}".format(current_time),
+    # mkdir_dist = BashOperator(
+    #     task_id = "mkdir_dist",
+    #     bash_command = "hdfs dfs -mkdir /user/iabd2_group6/data/{}".format(current_time),
+    # ) 
+
+    # put_data_to_hdfs = BashOperator(
+    #     task_id = "put_data_to_hdfs",
+    #     bash_command = "hdfs dfs -moveFromLocal /tmp/data_groupe6/*.csv /user/iabd2_group6/data/{}".format(current_time),
+    # ) 
+
+    # put_src_to_hdfs = BashOperator(
+    #     task_id = "mkdir_dist",
+    #     bash_command = "hdfs dfs -rm /root/airflow/dags/dag_nougatine/src_app/* && hdfs dfs -moveFromLocal /root/airflow/dags/dag_nougatine/src_app/*.py /user/iabd2_group6/app/",
+    # ) 
+
+    # submit_t1 = BashOperator(
+    #     task_id = "submit_t1",
+    #     bash_command = "spark-submit --master yarn --deploy-mode cluster hdfs://d271ee89-3c06-4d40-b9d6-d3c1d65feb57.priv.instances.scw.cloud:8020/user/iabd2_group6/app/collect_from_spotify.py",
     ) 
 
-    put_to_hdfs = BashOperator(
-        task_id = "put_to_hdfs",
-        bash_command = "hdfs dfs -moveFromLocal /tmp/data_groupe6/*.csv /user/iabd2_group6/data/{}".format(current_time),
-    ) 
+#[install, mkdir] >> get_data >> mkdir_dist >> [put_data_to_hdfs, put_src_to_hdfs]>> [clean , submit_t1] 
 
-    get_data = PythonOperator(
-        task_id='get_data',
-        python_callable=get_data.launch,
-        op_kwargs={
-            'path':"/tmp/data_groupe6"
-        }
-    )
-    
-
-[install, mkdir] >> get_data >> mkdir_dist >> put_to_hdfs >> clean
+clean
