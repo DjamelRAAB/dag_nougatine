4424ccf9c464f441a588918ffbc0427308a951af
draab <djamel.r.75@gmail.com>
2020-12-09 22:59:55 +0100

    update

--- dag_groupe6.py
+++ dag_groupe6.py
@@ -69,7 +69,7 @@ with dag:
     # submit_t1 = BashOperator(
     #     task_id = "submit_t1",
     #     bash_command = "spark-submit --master yarn --deploy-mode cluster hdfs://d271ee89-3c06-4d40-b9d6-d3c1d65feb57.priv.instances.scw.cloud:8020/user/iabd2_group6/app/collect_from_spotify.py",
-    ) 
+    # ) 
 
 #[install, mkdir] >> get_data >> mkdir_dist >> [put_data_to_hdfs, put_src_to_hdfs]>> [clean , submit_t1] 
 
