50239472fa97651cc4d98dd9f477ec02ce926b78
draab <djamel.r.75@gmail.com>
2020-12-09 21:52:27 +0100

    update

--- dag_groupe6.py
+++ dag_groupe6.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-#from apispotify import get_data
+from apispotify import get_data
 
 
 def hello_world(arg1): 
@@ -30,24 +30,23 @@ with dag:
         bash_command = "pip install /root/airflow/dags/dag_nougatine/.",
     ) 
 
-    # mkdir = BashOperator(
-    #     task_id = "mkdir",
-    #     bash_command = "mkdir /tmp/data_groupe6",
-    # ) 
+    mkdir = BashOperator(
+        task_id = "mkdir",
+        bash_command = "mkdir /tmp/data_groupe6",
+    ) 
 
-    # ls = BashOperator(
-    #     task_id = "ls",
-    #     bash_command = "ls -al /tmp/data_groupe6",
-    # )
+    ls = BashOperator(
+        task_id = "ls",
+        bash_command = "ls -al /tmp/data_groupe6",
+    )
     
-    # get_data = PythonOperator(
-    #     task_id='get_data',
-    #     python_callable=get_data.launch,
-    #     op_kwargs={
-    #         'path':"/tmp/data_groupe6"
-    #     }
-    # )
+    get_data = PythonOperator(
+        task_id='get_data',
+        python_callable=get_data.launch,
+        op_kwargs={
+            'path':"/tmp/data_groupe6"
+        }
+    )
     
-#git echo_hello_world >>  install >> python_hello_world >> 
-#mkdir >> get_data >> ls 
-install
+
+install >> mkdir >> get_data >> ls
