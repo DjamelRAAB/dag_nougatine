2df9f001066308f77c5142b7d6ee7101f2a5d83c
draab <djamel.r.75@gmail.com>
2020-12-09 22:20:11 +0100

    update

--- dag_groupe6.py
+++ dag_groupe6.py
@@ -3,7 +3,10 @@ from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from apispotify import get_data
+import datetime as dt 
 
+now = dt.datetime.now() #- dt.timedelta(days=7)
+current_time = now.strftime('%Y%m%d')
 
 def hello_world(arg1): 
     print("Hello {} by PythonOperator".format(arg1))
@@ -25,15 +28,15 @@ dag = DAG ( 'dag_groupe6',
 
 with dag:
 
-    # install = BashOperator(
-    #     task_id = "install",
-    #     bash_command = "pip install /root/airflow/dags/dag_nougatine/.",
-    # ) 
+    install = BashOperator(
+        task_id = "install",
+        bash_command = "pip install /root/airflow/dags/dag_nougatine/.",
+    ) 
 
-    # mkdir = BashOperator(
-    #     task_id = "mkdir",
-    #     bash_command = "mkdir /tmp/data_groupe6",
-    # ) 
+    mkdir = BashOperator(
+        task_id = "mkdir",
+        bash_command = "mkdir /tmp/data_groupe6",
+    ) 
 
     clean = BashOperator(
         task_id = "clean",
@@ -42,18 +45,16 @@ with dag:
     
     put_to_hdfs = BashOperator(
         task_id = "put_to_hdfs",
-        bash_command = "hdfs dfs -moveFromLocal /tmp/data_groupe6/*.csv /user/iabd2_group6/data/",
+        bash_command = "hdfs dfs -moveFromLocal /tmp/data_groupe6/*.csv /user/iabd2_group6/data/{}".format(current_time),
     ) 
 
-    # get_data = PythonOperator(
-    #     task_id='get_data',
-    #     python_callable=get_data.launch,
-    #     op_kwargs={
-    #         'path':"/tmp/data_groupe6"
-    #     }
-    # )
+    get_data = PythonOperator(
+        task_id='get_data',
+        python_callable=get_data.launch,
+        op_kwargs={
+            'path':"/tmp/data_groupe6"
+        }
+    )
     
 
-#install >> mkdir >> get_data >> ls
-
-put_to_hdfs >> clean
+install >> mkdir >> get_data >> put_to_hdfs >> clean
