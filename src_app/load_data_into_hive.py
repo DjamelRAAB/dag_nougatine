a0a30767e3ec283652b270ec7988cce98bdd9ab6
Anis Barr <barrma@hotmail.fr>
2020-12-10 07:56:46 +0000

    update

--- src_app/load_data_into_hive.py
+++ src_app/load_data_into_hive.py
@@ -75,7 +75,7 @@ def main():
   list_name = ["albums_releases_full","artists_releases_full","tracks_featured_full","tracks_releases_full","playlists_featured_full","artists_featured_full"]
   for name in list_name :
         exec('df_'+ name +' = load_csv_file(log,spark,"/user/iabd2_group6/data/'+date+'/df_'+name+'.csv")')
-        exec('save_in_hive(log,spark,df_'+ name +' ,"tb_'+name+'_v1",date )')
+        # exec('save_in_hive(log,spark,df_'+ name +' ,"tb_'+name+'_v1",date )')
   df_artist_up = get_top_artist_from_top_playlist(log,df_artists_releases_full).coalesce()
   df_artist_already_up = get_top_artist_from_top_playlist(log,df_artists_featured_full).coalesce()
 
